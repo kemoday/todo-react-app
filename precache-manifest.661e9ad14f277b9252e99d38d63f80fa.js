4cfc115a6a0715efb5fbe52b08af0041b866bda9
Mohamed Awde <kemoday@gmail.com>
2020-12-30 22:34:20 +0200

    Updates

---
+++ precache-manifest.661e9ad14f277b9252e99d38d63f80fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00d66accf5de6963e361d7ff7bf8382",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "97c0fc9526023dfea31c",
+    "url": "/todo-react-app/static/css/main.a14ba1d4.chunk.css"
+  },
+  {
+    "revision": "8e19a0c8bd5d0f9c30e3",
+    "url": "/todo-react-app/static/js/2.106fedc6.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/todo-react-app/static/js/2.106fedc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c0fc9526023dfea31c",
+    "url": "/todo-react-app/static/js/main.050d1f83.chunk.js"
+  },
+  {
+    "revision": "281aa049b85c98bd64f1",
+    "url": "/todo-react-app/static/js/runtime-main.7d9b64d6.js"
+  },
+  {
+    "revision": "5eb938e48ae65421d69a63d8c43474b7",
+    "url": "/todo-react-app/static/media/add-new-task.5eb938e4.jpg"
+  }
+]);
